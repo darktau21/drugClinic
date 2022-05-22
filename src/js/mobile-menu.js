f342033461cca87b71a792e6fe932c366a15c621
Kirill Naumenko <naumenkokirill466@gmail.com>
2022-05-22 19:45:08 +0300

    Заготовка для мобильного меню

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,11 @@
+export default class MobileMenu {
+  constructor(btnClass, menuClass, activeClass) {
+    this.btn = btnClass;
+    this.menuClass = menuClass;
+    this.activeClass = activeClass;
+  }
+
+  init() {
+
+  }
+}
